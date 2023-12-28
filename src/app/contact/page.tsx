ec15229dee4921eaa156529353aec5fb8814d76d
Phạm Long <longpd3.avitex@gmail.com>
2023-12-28 22:10:42 +0700

    update

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,4 @@
-'use client'
-
-import React, { useState } from 'react'
+import React from 'react'
 import Link from 'next/link';
 import TopNavTwo from '@/components/Headers/TopNav/TopNavTwo'
 import MenuTwo from '@/components/Headers/Menu/MenuTwo'
